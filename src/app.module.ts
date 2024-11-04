ab90589e3d844a4730eeac568ac31bb0af6e8db8
hani8092 <hani.patel@sattvabit.com>
2024-11-04 13:37:54 +0530

    feat: added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlaylistsModule } from './playlists/playlists.module';
 import { SongsModule } from './songs/songs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost:27017/music-playlist-management',
-    ),
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     UsersModule,
     PlaylistsModule,
     SongsModule,
